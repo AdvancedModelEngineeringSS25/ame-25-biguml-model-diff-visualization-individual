58bfe80b2e95ebb714abf88c051bdf802c9c541b
Andreas Hell <andhell03@gmail.com>
2025-05-07 22:01:12 +0200

    Ensure no temporary files on save

--- client/packages/big-diff-visualization/src/vscode/diff.handler.ts
+++ client/packages/big-diff-visualization/src/vscode/diff.handler.ts
@@ -106,14 +106,45 @@ export class DiffActionHandler implements Disposable {
                     const tempUnotationUri = vscode.Uri.parse(message.action.path.replace(/\.uml$/, '.unotation'));
 
                     // Delete the files via the vscode API
-                    vscode.workspace.fs.delete(tempUmlUri);
-                    vscode.workspace.fs.delete(tempUnotationUri);
+                    this.silentDelete(tempUmlUri);
+                    this.silentDelete(tempUnotationUri);
+
+                    // Because of the stateful behavior of the GLSP server and its authority to save files, it saves
+                    // the temporary file again on SaveModelAction
+                    // Therefore these watchers are instated to immediately delete those files again
+                    // This is a necessary measure to deal with imo improper server behavior
+                    const watcherUml = vscode.workspace.createFileSystemWatcher(
+                        new vscode.RelativePattern(
+                            vscode.workspace.getWorkspaceFolder(tempUmlUri) ?? '',
+                            vscode.workspace.asRelativePath(tempUmlUri)
+                        )
+                    );
+                    watcherUml.onDidCreate(this.silentDelete);
+                    this.toDispose.push(watcherUml);
+
+                    const watcherUnotation = vscode.workspace.createFileSystemWatcher(
+                        new vscode.RelativePattern(
+                            vscode.workspace.getWorkspaceFolder(tempUnotationUri) ?? '',
+                            vscode.workspace.asRelativePath(tempUnotationUri)
+                        )
+                    );
+                    watcherUnotation.onDidCreate(this.silentDelete);
+                    this.toDispose.push(watcherUnotation);
 
                     // The Response doesn't serve a particular purpose except cleanly finishing the action cycle
                     return DeleteLastCommitModelFileActionResponse.create();
                 }
             )
         );
+    }
+
+    private silentDelete(uri: vscode.Uri) {
+        try {
+            vscode.workspace.fs.delete(uri, { useTrash: false });
+        } catch (error) {
+            // The possible errors of delete are ignored as the usage is already safe
+            // Possibly missing files are expected
+        }
     }
 
     dispose(): void {
